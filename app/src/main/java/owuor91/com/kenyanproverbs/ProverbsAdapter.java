54063dab69f00f0aa86dcaaa8a2906bb01afcb10
John Owuor <owuor91@gmail.com>
2016-03-24 17:34:55 +0300

    create proverb adapter

---
+++ app/src/main/java/owuor91/com/kenyanproverbs/ProverbsAdapter.java
@@ -0,0 +1,33 @@
+package owuor91.com.kenyanproverbs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by johno_000 on 3/24/2016.
+ */
+public class ProverbsAdapter extends ArrayAdapter<Proverb> {
+
+    public ProverbsAdapter(Context context, ArrayList<Proverb> proverbs){
+        super(context,0,proverbs);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Proverb proverb = getItem(position);
+
+        if (convertView==null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
+        }
+
+        TextView txtProverb = (TextView)convertView.findViewById(R.id.helloText);
+        txtProverb.setText(proverb.text);
+        return convertView;
+    }
+}
