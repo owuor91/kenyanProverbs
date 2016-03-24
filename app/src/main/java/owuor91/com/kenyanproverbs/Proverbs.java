62d164269e9019a4002272df63777aeff99897fe
John Owuor <owuor91@gmail.com>
2016-03-24 12:32:45 +0300

    create item layout and array from resources

--- app/src/main/java/owuor91/com/kenyanproverbs/Proverbs.java
+++ app/src/main/java/owuor91/com/kenyanproverbs/Proverbs.java
@@ -6,6 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Proverbs extends AppCompatActivity {
 
@@ -16,7 +23,15 @@ public class Proverbs extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
+        String[] provArray = getResources().getStringArray(R.array.proverbsArray);
+        List<String> provArrayList = Arrays.asList(provArray);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, R.layout.item, R.id.helloText, provArrayList);
+
+        flingContainer.setAdapter(arrayAdapter);
+        
     }
 
 }
